919f1b7c45406a9d629edc2d023a7eaeb1479318
Berat Yilmaz <beratyilmaz3102@gmail.com>
2023-07-08 00:53:04 +0200

    chore: Complete general set-up of the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <h1 className={"py-4 font-bold bg-slate-600"}> Project setup</h1>
+      <Button variant="contained" className={"bg-green-500"}>
+        Test
+      </Button>
+    </>
+  );
+}
+
+export default App;
